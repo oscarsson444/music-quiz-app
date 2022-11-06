766a71283cf97b8216c2cb16070e2dd0c9f6c42c
oscarsson444 <oscarsson444@gmail.com>
2022-11-06 18:13:58 +0100

    Can fetch video from yt with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactPlayer from "react-player/youtube";
 
 function App() {
+  const [videoUrl, setVideoUrl] = useState("");
+  const videoName = encodeURIComponent("Smells like teen spirit");
+  const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB5T0IPe7RxSZ2I1YWXA7THAtwvrl2dgvM&q=${videoName}&type=video&part=snippet&maxResults=1`;
+
+  const searchButton = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    const videoId = data.items[0].id.videoId;
+    const vidUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    setVideoUrl(vidUrl);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={searchButton}>Search!</button>
+      <ReactPlayer url={videoUrl} />
     </div>
   );
 }
